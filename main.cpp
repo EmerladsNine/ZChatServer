c125f876ba9ddac46c4b15027419cc68c03c2ef4
UZMON <nar657965@gmail.com>
2025-12-31 14:51:08 +0200

    multi client update

--- main.cpp
+++ main.cpp
@@ -1,85 +1,101 @@
+#include <algorithm>
+#include <arpa/inet.h>
 #include <asm-generic/socket.h>
-#include <cstdlib>
 #include <cstddef>
 #include <cstdio>
-#include <iostream>
-#include <string>
+#include <cstdlib>
 #include <cstring>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <iostream>
 #include <netinet/in.h>
+#include <string>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
 
-int main()
-{
-	const int PORT = 9999;
-	
-	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd < 0) {
-		perror("socket");
-		return 1;
-	}
+int main() {
+  const int PORT = 9999;
 
-	int opt = 1;
-	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) {
+    perror("socket");
+    return 1;
+  }
 
-	sockaddr_in addr{};
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(PORT);
+  int opt = 1;
+  setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-	if (bind(server_fd, (sockaddr*)&addr, sizeof(addr)) < 0) {
-		perror("bind");
-		return 1;
-	}
+  sockaddr_in addr{};
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons(PORT);
 
-	if (listen(server_fd, 10) < 0) {
-		perror("listen");
-		return 1;
-	}
+  if (bind(server_fd, (sockaddr *)&addr, sizeof(addr)) < 0) {
+    perror("bind");
+    return 1;
+  }
 
-	std::cout << "Server Listening on port " << PORT << "\n";
+  if (listen(server_fd, 10) < 0) {
+    perror("listen");
+    return 1;
+  }
 
-	while (true) {
-		sockaddr_in client_addr{};
-		socklen_t client_len = sizeof(client_addr);
+  std::cout << "Server Listening on port " << PORT << "\n";
+  std::vector<int> clientsConnected;
+  while (true) {
+    fd_set readfds;    // The set of clients ready to be read.
+    FD_ZERO(&readfds); // Clear all bits
+    FD_SET(server_fd, &readfds);
 
-		int client_fd = accept(server_fd, (sockaddr*)&client_addr, &client_len);
-		if (client_fd < 0) 
-		{
-			perror("Accept");
-			continue;
-		}
-		std::cout << "Accepted \n";
+    // get max fd
+    int max_fd = server_fd;
+    for (int c : clientsConnected) {
+      FD_SET(c, &readfds);
+      max_fd = std::max(max_fd, c);
+    }
 
-		std::string buffer;
-		char temp[1024];
+    // waits until at least one socket is readable and removes every not
+    // readable socket from readfds
+    std::cout << "waiting ..." << std::endl;
+    if (select(max_fd + 1, &readfds, nullptr, nullptr, nullptr) < 0) {
+      perror("select");
+      break;
+    }
+    std::cout << "a socket is readable" << std::endl;
 
-		while (true) {
-			ssize_t bytes = recv(client_fd, temp, sizeof(temp), 0);
-			if (bytes <= 0) {
-				break;
-			}
+    // if server socket is readable then there is a new client connects
+    if (FD_ISSET(server_fd, &readfds)) {
+      int client_fd = accept(server_fd, nullptr, nullptr);
+      if (client_fd >= 0) {
+        std::cout << "New Client Connected : " << client_fd << std::endl;
+        clientsConnected.push_back(client_fd);
+      }
+    }
 
-			buffer.append(temp,bytes);
-			size_t pos;
-			while ((pos = buffer.find('\n')) != std::string::npos) {
-				std::string line = buffer.substr(0,pos);
-				buffer.erase(0,pos + 1);
-				
-				std::cout << "Recieved: "<< line << std::endl;
-		std::string jsonData = "{\"session\":\"default\",\"chatId\":\"120363389705652096@g.us\",\"text\":\"" + line + "\"}";
-		    std::string curlCommand = "curl -X POST \"http://localhost:3000/api/sendText\" "
-                              "-H \"accept: application/json\" "
-                              "-H \"Content-Type: application/json\" "
-                              "-H \"X-Api-Key: 01fea06d176a40e2a269942fa3f8578d\" "
-                              "-d '" + jsonData + "'";
+    char buffer[1024];
+    for (auto it = clientsConnected.begin(); it != clientsConnected.end();it++) {
+      int client = *it;
+      std::cout << "Checking Client :" << client << std::endl;
+      if (FD_ISSET(client, &readfds)) {
+        std::cout << "a client is readable" << client << std::endl;
+        ssize_t n = recv(client, buffer, sizeof(buffer), 0);
+        if (n <= 0) {
+          std::cout << "Client disconnected : " << client << "\n";
+          close(client);
+          it = clientsConnected.erase(it);
+          continue;
+        }
 
-				system(curlCommand.c_str());
-				const char* reply = "OK\n";
-				send(client_fd,reply, strlen(reply),0);
-			}
-		}
-	}
-
+        std::cout << "BroadCasting... from " << client << std::endl;
+        // Broadcast to all other clients
+        for (int other : clientsConnected) {
+          if (other != client) {
+            send(other, buffer, n, 0);
+            std::cout << "BroadCasting to " << other << std::endl;
+          }
+        }
+      }
+    }
+  }
 }
