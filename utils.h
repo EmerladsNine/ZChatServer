263e65e2431d1172c3ebfc256c341325061e3b7e
Jawad Zaraket <emeraldsnine@gmail.com>
2026-01-25 17:46:01 +0200

    c++ errors sorry guys another 10m$ lost

--- utils.h
+++ utils.h
@@ -4,4 +4,12 @@
 uint16_t readUint16FromBuffer(std::vector<char> &buffer);
 
 template <typename T>
-std::vector<char> intToBigEndian(T num);
+std::vector<char> intToBigEndian(T num) {
+
+    std::vector<char> list;
+    for (int i = sizeof(T) - 1; i != -1; i--) {
+        list.push_back(static_cast<char>((num >> (i * 8)) & 0xff));
+    }
+
+    return list;
+}
